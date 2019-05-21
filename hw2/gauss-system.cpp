1dd485c20f63abcc362433801c6f5cc476b25e5b
AntiFrizz1 <gluhov1998@yandex.ru>
2019-05-22 02:11:09 +0300

    rename file + fix gauss

--- hw2/gauss-system.cpp
+++ hw2/gauss-system.cpp
@@ -63,10 +63,7 @@ int main() {
     }
     system = solve(system);
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n + 1; j++) {
-            cout << system[i][j] << ' ';
-        }
-        cout << endl;
+        cout << system[i][n] << ' ';
     }
     return 0;
 }
