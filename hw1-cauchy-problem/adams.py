caba01f9058fbac5841983820a8d46955182b841
zelenoff4 <zelenoff9988@gmail.com>
2019-10-23 12:28:01 +0300

    fix

--- hw1-cauchy-problem/adams.py
+++ hw1-cauchy-problem/adams.py
@@ -11,12 +11,15 @@ def adams_method(f, h, n, vec0, r, sigma=10, b=8 / 3):
     f0 = f(vec_0, sigma, b, r)
     vec_1_ = runge_kutta.runge_kutta(f, h, 1, vec_0, r, sigma, b)
     vec_1 = [vec_1_[0][0], vec_1_[1][0], vec_1_[2][0]]
+    lib.transpose_and_add(ans, vec_1)
     f1 = f(vec_1, sigma, b, r)
     vec_2_ = runge_kutta.runge_kutta(f, h, 1, vec_1, r, sigma, b)
     vec_2 = [vec_2_[0][0], vec_2_[1][0], vec_2_[2][0]]
+    lib.transpose_and_add(ans, vec_2)
     f2 = f(vec_2, sigma, b, r)
     vec_3_ = runge_kutta.runge_kutta(f, h, 1, vec_2, r, sigma, b)
     vec_3 = [vec_3_[0][0], vec_3_[1][0], vec_3_[2][0]]
+    lib.transpose_and_add(ans, vec_3)
     f3 = f(vec_3, sigma, b, r)
     for i in range(n):
         vec_4_0 = lib.sum_vectors(vec_3,
@@ -32,23 +35,24 @@ def adams_method(f, h, n, vec0, r, sigma=10, b=8 / 3):
                                 )
         f_4_0 = f(vec_4_0, sigma, b, r)
 
-        # vec_4 = lib.sum_vectors(vec_3,
-        #                         lib.num_to_vector(h / 24,
-        #                                           lib.sum_vectors(lib.num_to_vector(9, f_4_0),
-        #                                                           lib.sum_vectors(lib.num_to_vector(19, f3),
-        #                                                                           lib.sum_vectors(lib.num_to_vector(-5, f2),
-        #                                                                                           f1
-        #                                                                                           )
-        #                                                                           )
-        #                                                           )
-        #                                           )
-        #                         )
+        vec_4 = lib.sum_vectors(vec_3,
+                                lib.num_to_vector(h / 24,
+                                                  lib.sum_vectors(lib.num_to_vector(9, f_4_0),
+                                                                  lib.sum_vectors(lib.num_to_vector(19, f3),
+                                                                                  lib.sum_vectors(lib.num_to_vector(-5, f2),
+                                                                                                  f1
+                                                                                                  )
+                                                                                  )
+                                                                  )
+                                                  )
+                                 )
+        f4 = f(vec_4, sigma, b, r)
         vec_0 = vec_1
         vec_1 = vec_2
-        vec_3 = vec_4_0
+        vec_3 = vec_4
         f0 = f1
         f1 = f2
-        f3 = f_4_0
-        lib.transpose_and_add(ans, vec_4_0)
+        f3 = f4
+        lib.transpose_and_add(ans, vec_4)
 
     return ans
--- hw1-cauchy-problem/data.py
+++ hw1-cauchy-problem/data.py
@@ -1,6 +1,20 @@
+import math
+
 def lorenz(vec, s, b, r):
     assert len(vec) == 3
     x = s * (vec[1] - vec[0])
     y = r * vec[0] - vec[1] - vec[0] * vec[2]
     z = vec[0] * vec[1] - b * vec[2]
     return [x, y, z]
+
+
+def first_point():
+    return [0, 0, 0]
+
+
+def second_point(b, r):
+    return [(b * (r - 1)) ** 0.5, (b * (r - 1)) ** 0.5, r - 1]
+
+
+def third_point(b, r):
+    return [-(b * (r - 1)) ** 0.5, -(b * (r - 1)) ** 0.5, r - 1]
